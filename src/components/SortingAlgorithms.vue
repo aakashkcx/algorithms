a1a3117e7a6a0915bf490b228006bc1b42207f54
Aakash Kc <aakashkcx@gmail.com>
2020-05-17 02:23:15 +0100

    bubbleSort implementation

--- src/components/SortingAlgorithms.vue
+++ src/components/SortingAlgorithms.vue
@@ -3,17 +3,30 @@
     <h1>Sorting Algorithms</h1>
     <Display :array="array" />
     <div>
+      <button @click="newArray">New Array</button>
+    </div>
+    <div>
       <button @click="bubbleSort">Bubble Sort</button>
       <button @click="insertionSort">Insertion Sort</button>
       <button @click="selectionSort">Selection Sort</button>
       <button @click="mergeSort">Merge Sort</button>
-      <button @click="newArray">New Array</button>
+    </div>
+    <div v-if="list">
+      <button @click="start">Start</button>
+      <button @click="previous">Previous</button>
+      <button @click="next">Next</button>
+      <button @click="end">End</button>
+    </div>
+    <div>
+      <button>Play</button>
+      <button>Pause</button>
     </div>
   </div>
 </template>
 
 <script>
 import Display from "./SortingAlgorithms/components/Display";
+import { bubbleSort } from "./SortingAlgorithms/sorts";
 import { newArray } from "./SortingAlgorithms/util";
 
 export default {
@@ -23,21 +36,32 @@ export default {
 
   data() {
     return {
-      array: Array(100).fill(),
+      array: newArray(100),
       list: null,
+      interval: null,
     };
-  },
-
-  created() {
-    this.newArray();
   },
 
   methods: {
     newArray: function() {
       this.array = newArray(100);
+      this.list = null;
+    },
+    start: function() {
+      if (this.list.head) this.array = this.list.head;
+    },
+    end: function() {
+      if (this.list.tail) this.array = this.list.tail;
+    },
+    next: function() {
+      if (this.array.next) this.array = this.array.next;
+    },
+    previous: function() {
+      if (this.array.previous) this.array = this.array.previous;
     },
     bubbleSort: function() {
-      this.array = this.array.sort((a, b) => a.value - b.value);
+      this.list = bubbleSort(this.array);
+      this.array = this.list.head;
     },
     insertionSort: function() {
       this.array = this.array.sort((a, b) => a.value - b.value);
--- src/components/SortingAlgorithms/sorts.js
+++ src/components/SortingAlgorithms/sorts.js
@@ -1,7 +1,18 @@
 import ArrayLinkedList from "./ArrayLinkedList";
 
-export const bubbleSort = () => {
-  return new ArrayLinkedList();
+export const bubbleSort = (array) => {
+  let list = new ArrayLinkedList();
+  array = [...array];
+  list.add([...array]);
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 1; j < array.length; j++) {
+      if (array[j].value < array[j - 1].value) {
+        [array[j], array[j - 1]] = [array[j - 1], array[j]];
+        list.add([...array]);
+      }
+    }
+  }
+  return list;
 };
 export const insertionSort = () => {
   return new ArrayLinkedList();
