96e739a6a86ddfc8ce37f0f9d92bf3b102cb5b21
Aakash Kc <aakashkcx@gmail.com>
2020-10-28 00:25:08 +0000

    add home nav buttons

--- src/Home/index.vue
+++ src/Home/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1>Algorithms</h1>
+    <div id="buttons">
+      <router-link to="/sorting">Sorting</router-link>
+      <router-link to="/pathfinding">Pathfinding</router-link>
+    </div>
   </div>
 </template>
 
@@ -10,4 +14,15 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#buttons {
+  margin-top: 4em;
+}
+#buttons a {
+  display: inline-block;
+  padding: 1em 2em;
+  margin: 0 1em;
+  border: solid black;
+  text-decoration: none;
+}
+</style>
